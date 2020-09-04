fc4816857588944a614a9a113186a93080c5ba77
ericjt505 <weirdspacetobe@gmail.com>
2020-09-03 23:04:54 -0400

    Adding my playground file to Github.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var coolWords = "Hello, playground"
+
+var integer = 10
+var doubleTester: Double = 10.053242
+
+var string2 = "String always exist in quotes"
+
+var integer2 = 20
+
+coolWords
+
+integer
+
+integer2
+
+integer + integer2
+integer * integer2
+integer2 / integer
+
+coolWords + string2
+coolWords + String(integer)
